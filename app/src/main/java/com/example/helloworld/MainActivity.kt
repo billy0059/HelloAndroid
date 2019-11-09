e6933e26a98460a61ad93a744d42e31a31b2158a
Billy.Wang <billy0059.bw@gmail.com>
2019-11-09 18:22:41 +0800

    Linear/Relative Layout Practice : 1. Change activity_main.xml to LinearLayout, add some data for ListView from MainActivity.kt 2. Add a RelativeLayout (app/src/main/res/layout/add_account.xml) (no use)
    Referenced tutorial : http://www.codedata.com.tw/mobile/atk0202/

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val data = arrayOf("關於Android Tutorial的事情",
+            "一隻非常可愛的小狗狗!", "一首非常好聽的音樂！")
+        val layoutId = android.R.layout.simple_list_item_1
+        val adapter = ArrayAdapter(this, layoutId, data)
+
+        val itemList: ListView = findViewById(R.id.item_list) // find android:id from res.layout folder.
+        itemList.setAdapter(adapter)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
